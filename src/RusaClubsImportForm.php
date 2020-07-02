2d9a64e53876cc092d8d5f933b386836c9edbc08
Paul Lieberman <lieb77@gmail.com>
2020-07-01 20:27:07 -0700

    Initial commit

---
+++ src/RusaClubsImportForm.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace Drupal\rusa_clubs_import;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\taxonomy\Entity\Term;  
+use Drupal\node\Entity\Node;
+use Drupal\rusa_api\RusaClubs;
+
+
+class RusaClubsImportForm extends FormBase {
+
+  // Required function
+  public function getFormId() {
+    return 'rusa_clubs_import_form';
+  }
+
+
+  public function buildForm(array $form, FormStateInterface $form_state) {
+
+    $form['clubs'] = [
+      '#type'   => 'item',
+      '#markup' => $this->t("Click the Import button to begin the transfer of Club data into Drupal"),
+    ];
+
+    $form['actions']['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Import'),
+    ];
+
+    $form['#attributes']['class'][] = 'rusa-form';
+    $form['#attached']['library'][] = 'rusa_api/rusa_style';
+
+    return $form;
+  }
+
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+
+    $clobj  = new RusaClubs();
+    $clubs  = $clobj->getClubsArray();
+	
+		// Setup batch process
+		$batch = [
+    	'title' => $this->t('Importing clubs'),
+      'operations' => [['Drupal\rusa_clubs_import\RusaClubsImportForm::batchStart', ['clubs']]],
+      'finished'   => 'Drupal\rusa_clubs_import\RusaClubsImportForm::batchFinished',
+    ];
+
+    foreach ($clubs as $club) {
+      $batch['operations'][] =  ['Drupal\rusa_clubs_import\RusaClubsImportForm::batchProcess', [$club]];
+    }
+
+		batch_set($batch);
+	}
+
+	public static function batchStart($clubs, &$context) {
+      $context['results']['updates'] = 0;
+      $context['results']['additions'] = 0;
+  }
+
+
+	public static function batchprocess($club, &$context) {
+		if ($club->name === "Randonneurs USA" ||		
+        $club->name === "International Randonneurs"  ||
+        $club->name === "Independent"  ||
+        $club->acpcode < 900000 ) {
+        
+      $context['message'] = t("Skipping " . $club->name);
+      return;
+		}
+    
+    // Check to see if club exists
+    $result = \Drupal::entityQuery('node')
+      ->condition('type', 'club')
+      ->condition('field_club_acpcode', $club->acpcode, '=' )
+      ->execute();
+    if (!empty($result)) { 
+    $nid = array_shift($result); 
+      $node_storage = \Drupal::entityTypeManager()->getStorage('node');
+      $node = $node_storage->load($nid);
+	    $context['message'] = t("Updating club " . $club->name);
+      $context['results']['updates']++ ;
+    }
+    else {
+    	$node = Node::create(['type' => 'club']);
+    	$node->set('title', $club->name);
+      $node->enforceIsNew();
+      $node->save();
+      
+	    $context['message'] = t("Adding club " . $club->name);
+      $context['results']['additions']++ ;
+    }
+
+    // Now set other values
+    $node->set('field_club_acpcode', $club->acpcode);
+    $node->set('field_club_status', $club->status == 'A' ? TRUE : FALSE);
+    $node->set('field_club_date_added', preg_replace("/\//",  "-", $club->date));
+    $node->set('body', [
+      	'value'  => $club->notes,
+      	'format' => 'basic_html',
+      ]);
+	
+    // Set the State taxonomy reference
+		preg_match("/9(\d\d)/", $club->acpcode, $matches);
+		if (isset($matches[1])) {
+     	$state_code = $matches[1];
+      $entity_id = \Drupal\rusa_clubs_import\RusaClubsImportForm::getState($state_code);
+      if (!empty($entity_id)) { 
+				$node->set('field_club_state', $entity_id);
+			}
+		}
+
+    $node->save();
+    
+		
+  }
+
+	public static function batchFinished($success, $results, $operations) {
+    $messenger = \Drupal::service('messenger');
+
+    if ($success) {
+      $messenger->addMessage(t('@count clubs added.', [ '@count' => $results['additions'] ]));
+		  $messenger->addMessage(t('@count clubs updated.', [ '@count' => $results['updates'] ]));
+	  }
+    else {
+      $error_operation = reset($operations);
+      $messenger->addMessage(t('An error occurred while processing @operation with arguments : @args', [
+              '@operation' => $error_operation[0],
+              '@args' => print_r($error_operation[0]),
+        ]));
+    }
+  }
+
+
+    public static function getState($code) {
+    	$result = \Drupal::entityQuery('taxonomy_term')
+      	->condition('vid', 'states')
+      	->condition('field_state_code', $code, "=")
+      	->execute();
+    	if (!empty($result)) {
+      	return  array_shift($result);
+			}
+  }
+  
+}
